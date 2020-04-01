12f0b2984302b9f1243fa1e68c489a899b542eb7
benjiboy <ben.burdick91@gmail.com>
2020-04-01 13:30:24 -0500

    Initial commit

---
+++ src/main/java/org/hospitalityprogram/furnituredonations/FurnitureDonationsApplication.java
@@ -0,0 +1,13 @@
+package org.hospitalityprogram.furnituredonations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureDonationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnitureDonationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hospitalityprogram/furnituredonations/FurnitureDonationsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hospitalityprogram.furnituredonations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureDonationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
